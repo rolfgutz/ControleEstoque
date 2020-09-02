f397995359013347f58fbe3f9593e54815d46ae0
rolf <rolf.junior@aluno.faculdadeimpacta.com.br>
2020-09-01 22:55:15 -0300

    iniciando git

---
+++ ControleDeEstoque.Web/Controllers/OperacaoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleDeEstoque.Web.Controllers
+{
+    public class OperacaoController : Controller
+    {
+        // GET: Operacao
+        public ActionResult EntradaEstoque()
+        {
+            return View();
+        }
+        public ActionResult SaidaEstoque()
+        {
+            return View();
+        }
+        public ActionResult LancaPerdaProduto()
+        {
+            return View();
+        }
+        public ActionResult Inventario()
+        {
+            return View();
+        }
+
+    }
+}
